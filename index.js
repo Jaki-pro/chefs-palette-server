157cd4c4d07bd6f57c5d5592d7ad2f0f9defd874
jaki <mdjak8980@gmail.com>
2023-12-04 19:44:22 +0600

    34

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 import express from 'express'
 import cors from 'cors'
-const app = express()
+const app = express() 
+import * as chefsData from 'file:///C:/react/firebase/Big%20Project/palette-server/data/chefs.json' assert { type: 'json' };
 app.use(cors()); 
 const port = 5000;  
 app.get('/',(req, res)=>{
     req='no-cors'
-    res.send({"ok":'okk'})
+    res.send(chefsData)
 })
 
 //Find chef using chefId
- 
 //Find recipes of Chef
 
 app.listen(5000, ()=>{
